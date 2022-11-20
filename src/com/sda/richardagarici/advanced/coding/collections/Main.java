14aa822d9f59075676a1148aa1c9e3493d4cdd6d
Richard Agarici <laurentiu.agarici@gmail.com>
2022-11-20 14:01:18 +0100

    no message

--- src/com/sda/richardagarici/advanced/coding/collections/Main.java
+++
@@ -1,7 +0,0 @@
-package com.sda.richardagarici.advanced.coding.collections;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
